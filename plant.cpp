bc13637713a149df93899f12084baf3a4287247e
dennisyong <taiwanese945@gmail.com>
2021-06-23 17:57:02 +0800

    123

--- plant.cpp
+++ plant.cpp
@@ -1,7 +1,162 @@
+#include<string>
+#include<iostream>
 #include"plant.h"
+
 using namespace std;
 
-plant::plant(string name,int cost,int maxhp,int atk) :name_(name), cost_(cost), maxhp_(maxhp), remainhp_(maxhp), atk_(atk){}
+int plant::healpoint_ = 20;
+
+string Hornflower::name_ = "Hornflower";
+
+int Hornflower::cost_ = 50;
+
+int Hornflower::maxhp_ = 50;
+
+int Hornflower::atk_ = 10;
+
+string Bombflower::name_ = "Bombflower";
+
+int Bombflower::cost_ = 50;
+
+int Bombflower::maxhp_ = 30;
+
+int Bombflower::used_ = 0;
+
+string Mushroom::name_ = "Mushroom";
+
+int Mushroom::cost_ = 25;
+
+int Mushroom::maxhp_ = 50;
+
+int Mushroom::maxtime_ = 2;
+
+int Mushroom::reward_ = 25;
+
+string Healflower::name_ = "Healflower";
+
+int Healflower::cost_ = 25;
+
+int Healflower::maxhp_ = 50;
+
+plant::plant(string name,int cost,int maxhp,int atk) :name_(name), cost_(cost), maxhp_(maxhp), remainhp_(maxhp), atk_(atk)
+{
+
+}
+
+plant::~plant()
+{
+
+}
+
+string plant::showname() const
+{
+    return name_;
+}
+
+int plant::showcost() const
+{
+    return cost_;
+}
+
+int plant::showmaxhp() const
+{
+    return maxhp_;
+}
+
+int plant::showhp() const
+{
+    return remainhp_;
+}
+
+int plant::showatk() const
+{
+    return atk_;
+}
+
+void plant::visit()  {cout << "a" << endl;}
+
+void plant::attacked(int atk)
+{
+    remainhp_ -= atk;
+}
+
+void plant::healed()
+{
+    remainhp_ += healpoint_;
+    if(remainhp_>maxhp_)
+    {
+        remainhp_ = maxhp_;
+    }
+}
+
+void plant::sethealpoint(int healpoint)
+{
+    healpoint_ = healpoint;
+}
+
+void Hornflower::set(string name, int cost, int hp, int atk)
+{
+    name_ = name;
+    cost_ = cost;
+    maxhp_ = hp;
+    atk_ = atk;
+}
+
+Hornflower::Hornflower(): plant(name_,cost_,maxhp_,atk_)
+{
+
+}
+
+
+void Bombflower::set(string name,int cost, int hp)
+{
+    name_ = name;
+    cost_ = cost;
+    maxhp_ = hp;
+}
+
+Bombflower::Bombflower(): plant(name_,cost_,maxhp_,maxhp_)
+{
+
+}
+
+Bombflower::~Bombflower()
+{
+    used_+=1;
+}
+
+int Bombflower::showused()
+{
+    return used_;
+}
+
+void Mushroom::set(string name, int cost, int hp, int time, int coin)
+{
+    name_ = name;
+    cost_ = cost;
+    maxhp_ = hp;
+    maxtime_ = time;
+    reward_ = coin;
+}
+
+Mushroom::Mushroom() : plant(name_,cost_,maxhp_,0)
+{
+
+}
+
+void Healflower::set(string name, int cost, int hp, int healpoint)
+{
+    name_ = name;
+    cost_ = cost;
+    maxhp_ = hp;
+    sethealpoint(healpoint);
+}
+
+Healflower::Healflower() : plant(name_,cost_,maxhp_,0)
+{
+
+}
+
 
 plant* create(char c)
 {
@@ -14,18 +169,30 @@ plant* create(char c)
     case 'B':
         temp = new Bombflower;
         break;
-    /*case 'C':
-
+    case 'C':
+        temp = new Mushroom;
         break;
     case 'H':
-
-        break;*/
+        temp = new Healflower;
+        break;
     }
     return temp;
 }
 
-ostream & operator << (ostream &os, const plant *rhs)
+bool alive (plant* pl)
+{
+    if(pl->showhp()<=0)
+    {
+        return false;
+    }
+    return true;
+}
+
+
+std::ostream & operator << (std::ostream &os, const plant *rhs)
 {
     os << rhs->showname() << " HP: " << rhs->showhp();
     return os;
 }
+
+
--- plant.h
+++ plant.h
@@ -1,71 +1,87 @@
-#include<string>
 #include<iostream>
-
 class plant
 {
 public:
     plant(std::string name,int cost,int maxhp,int atk);
-    //plant(std::string name,int cost,int maxhp,int atk) :name_(name), cost_(cost), maxhp_(maxhp), remainhp_(maxhp), atk_(atk){}
-    //plant(int cost,int maxhp,int atk) : cost_(cost), maxhp_(maxhp), remainhp_(maxhp), atk_(atk){}
-    virtual ~plant() {}
-    std::string showname() const {return name_;}
-    int showcost() const {return cost_;}
-    int showmaxhp() const {return maxhp_;}
-    int showhp() const {return remainhp_;}
-    int showatk() const {return atk_;}
-    virtual void visit()  {std::cout << "a" << std::endl;}
-    static constexpr int attacked = -15;
+    virtual ~plant();
+    std::string showname() const;
+    int showcost() const;
+    int showmaxhp() const;
+    int showhp() const;
+    int showatk() const;
+    virtual void visit();   //test
+    void attacked(int atk);
+    void healed();
 protected:
-
+    static void sethealpoint(int healpoint);
 private:
     std::string name_;
     int cost_;
     int maxhp_;
     int remainhp_;
     int atk_;
+    static int healpoint_;
 };
 
 class Hornflower : public plant
 {
 public:
-    static void set(std::string name, int cost, int hp, int atk){name_ = name,cost_ = cost,maxhp_ = hp,atk_ = atk;};
-    Hornflower(): plant(name_,cost_,maxhp_,atk_){}
-    virtual void visit() override {std::cout << "b" << std::endl;}
+    static void set(std::string name, int cost, int hp, int atk);
+    Hornflower();
+    //virtual void visit() override ;
+    virtual void visit() override {std::cout << "b" << std::endl;} //test
 private:
     static std::string name_;
     static int cost_;
     static int maxhp_;
     static int atk_;
 };
-std::string Hornflower::name_ = "Hornflower";
-int Hornflower::cost_ = 50;
-int Hornflower::maxhp_ = 50;
-int Hornflower::atk_ = 10;
-
-
 
 class Bombflower : public plant
 {
 public:
-    static void set(std::string name,int cost, int hp){name_ = name,cost_ = cost,maxhp_ = hp;};
-    Bombflower(): plant(name_,cost_,maxhp_,maxhp_){}
-    ~Bombflower() {used_+=1;}
-    static int showused() {return used_;};
+    static void set(std::string name,int cost, int hp);
+    Bombflower();
+    ~Bombflower() ;
+    static int showused() ;
 private:
     static std::string name_;
     static int cost_;
     static int maxhp_;
     static int used_;
 };
-std::string Bombflower::name_ = "Bombflower";
-int Bombflower::cost_ = 50;
-int Bombflower::maxhp_ = 30;
-int Bombflower::used_ = 0;
 
+class Mushroom : public plant
+{
+public:
+    static void set(std::string name, int cost, int hp, int time, int coin);
+    Mushroom();
+    //virtual void visit() override {std::cout << "c" << std::endl;}
+private:
+    static std::string name_;
+    static int cost_;
+    static int maxhp_;
+    static int maxtime_;
+    static int reward_;
+    int time = 0;
+};
+
+class Healflower : public plant
+{
+public:
+    static void set(std::string name, int cost, int hp, int healpoint);
+    Healflower();
+    //virtual void visit() override {std::cout << "c" << std::endl;}
+private:
+    static std::string name_;
+    static int cost_;
+    static int maxhp_;
+};
 
 plant* create(char c);
 
-
+bool alive (plant* pl);
 
 std::ostream & operator << (std::ostream &os, const plant *rhs);
+
 
