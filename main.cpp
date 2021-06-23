82af4d0442c1c3235f94b0909535bddcad13bb39
skyhong2002 <sky9109@gmail.com>
2021-06-23 15:04:20 +0800

    [update] arrange functions

--- main.cpp
+++ main.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 #include <algorithm>
 #include "map.h"
-#include "player.h"
-#include "zombie.h"
-#include "land.h"
 using namespace std;
 
 int main()
--- player.h
+++ player.h
@@ -6,7 +6,6 @@ class player
 {
 public:
     int Coin() const { return coin_; }
-    void Cost(const int i) { coin_ -= i; }
     int Position() const { return position_; }
     void Step(const int i, const int row) { position_ = (position_ + i) % row; }
     void Cost(const int m) { coin_ -= m; }
